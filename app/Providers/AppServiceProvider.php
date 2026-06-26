4da4051fd48628fd85d103908fdeda6e24a35c79
imam syuja <imamsyuja97@gmail.com>
2026-06-26 17:56:49 +0700

    fix: force HTTPS production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,7 +22,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         if (config('app.env') === 'production') {
-            \Illuminate\Support\Facades\Http::withoutVerifying();
+            \URL::forceScheme('https');
         }
     }
 }
